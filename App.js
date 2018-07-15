6b96e84a642d4029902f753723a2fc9b2288587e
Jakhongir Khusanov <worldj95@gmail.com>
2018-07-14 21:01:56 -0700

    Importing common  componentes

--- App.js
+++ App.js
@@ -3,15 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './app/reducers';
+import { Header } from './app/components/common'
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store = {createStore(reducers)}>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Header headerText = "Tech Stack" />
       </View>
       </Provider>
     );
@@ -22,7 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
