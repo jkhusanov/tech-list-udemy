512c442b7009f9136ce767b88b61ddb0551d0afa
Jakhongir Khusanov <worldj95@gmail.com>
2018-07-14 20:46:02 -0700

    Getting started with redux

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './app/reducers';
 
 export default class App extends React.Component {
   render() {
     return (
+      <Provider store = {createStore(reducers)}>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
+      </Provider>
     );
   }
 }
